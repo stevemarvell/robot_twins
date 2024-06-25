bb7e0c4606765f1ce5d3bdee1aaaad5c4dcc4548
Steve Marvell <stevemarvell@crucialtechnical.com>
2024-06-25 12:47:02 +0100

    simplify for a bit

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,35 @@
 use bevy::prelude::*;
 
-mod rdl;
-
-use std::fs;
-use rdl::parse_rdl;
-use crate::rdl::models::Robot;
-
-#[derive(Resource, Debug)]
-struct RobotTwin(Robot);
-
-impl From<Robot> for RobotTwin {
-    fn from(robot: Robot) -> Self {
-        RobotTwin(robot)
-    }
-}
-
 fn main() {
-    let rdl_content = fs::read_to_string("configs/hello_robot.rdl")
-        .expect("Unable to read rdl file");
-
-    let robot = match parse_rdl(&rdl_content) {
-        Ok(robot) => robot,
-        Err(e) => {
-            println!("Error parsing RDL: {}", e);
-            return;
-        }
-    };
-
-    let robot_twin: RobotTwin = robot.into();
-
     App::new()
-        .insert_resource(robot_twin)
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
+        .add_systems(Update, rotate_cube)
         .run();
 }
+
+#[derive(Component)]
+struct TheCube;
 
 fn setup(mut commands: Commands,
          mut materials: ResMut<Assets<StandardMaterial>>,
          mut meshes: ResMut<Assets<Mesh>>) {
+
+    // Add a camera
     commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(2.0, 5.0, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
+        transform: Transform::from_xyz(0.0, 5.0, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
     });
 
-    commands.spawn(PointLightBundle {
-        point_light: PointLight {
+    // Add a directional light
+    commands.spawn(DirectionalLightBundle {
+        directional_light: DirectionalLight {
             shadows_enabled: true,
-            ..Default::default()
+            ..default()
         },
-        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        transform: Transform::from_rotation(Quat::from_euler(
+            EulerRot::XYZ, -std::f32::consts::FRAC_PI_4, -std::f32::consts::FRAC_PI_4, 0.0,
+        )),
         ..default()
     });
 
@@ -70,14 +50,23 @@ fn setup(mut commands: Commands,
         ..default()
     });
 
-    // Spawn the torso part
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Cuboid::new(1.0, 1.0, 1.0)),
-        material: materials.add(StandardMaterial {
-            base_color: Color::RED,
+    // Add a cube
+    commands.spawn((
+        Name::new("TheCube"),
+        TheCube,
+        PbrBundle {
+            mesh: meshes.add(Cuboid::new(1.0, 1.0, 1.0)),
+            material: materials.add(StandardMaterial {
+                base_color: Color::rgb(0.0, 0.0, 1.0),
+                ..default()
+            }),
+            transform: Transform::from_xyz(0.0, 0.5, 0.0),
             ..default()
-        }),
-        transform: Transform::from_xyz(0.0, 0.5, 0.0),
-        ..default()
-    });
+        }));
+}
+
+fn rotate_cube(mut query: Query<&mut Transform, With<TheCube>>, time: Res<Time>) {
+    for mut transform in query.iter_mut() {
+        transform.rotation = transform.rotation * Quat::from_rotation_y(2.0 * time.delta_seconds());
+    }
 }
